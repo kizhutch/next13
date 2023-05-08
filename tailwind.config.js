fa55532d5ab77d6a02e48315a1a7f9c62383fa15
Kyle Hutchinson <khutchinson@extraspace.com>
2023-05-08 17:12:22 -0600

    added custom colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,18 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      //primary: a dark grey for text and accents
+      primary: "#3C3C3C",
+      //secondary: a light grey for background
+      secondary: "#F2F2F2",
+      //accent: a warm yellow for buttons and highlights
+      accent: "#FFCE00",
+      //cta: a teal green for call-to-action buttons
+      cta: "#127681",
+      //edges: a very light grey for borders and dividers
+      edges: "#F8F8F8"
+    }
   },
   plugins: [],
 }
